b4db1eee1b2dbc4c2d84b8357084661200a27261
Patrick LeBoutillier <patrick.leboutillier@gmail.com>
2021-08-26 13:51:09 -0400

    ok

--- chips/modules/scratch.py
+++ chips/modules/scratch.py
@@ -12,37 +12,32 @@ class scratch:
         self.data = data            # The data bus
         self.inst = None            # Must be set after initialisation
         self.index_reg = [0] * 16   # The actual registers
-        self.row_num = 0
-        self.row_even = 0 
-        self.row_odd = 0
-        self.data_in = 0
-        self.data_out = 0
+        self.row_num = 0            # The working row number
+        self.row_even = 0           # The even register in the working row
+        self.row_odd = 0            # The odd register in the working row
+        self.data_in = 0            # Data in from the bus
 
         self.timing = timing
 
         @A32clk2
-        def _():
-            if self.inst.sc:
-                self.row_even = self.index_reg[self.row_num * 2]
-                self.row_odd = self.index_reg[(self.row_num * 2) + 1]
-
-        @X12clk2
+        @X12clk2    # Set working row from the register array. There is a 0 override for FIN during X12.
         def _():
             if self.inst.sc:
                 row_num = self.row_num
-                if self.inst.fin():
-                    row_num = 0                  
+                if self.timing.x1():
+                    if self.inst.fin():
+                        row_num = 0  
                 self.row_even = self.index_reg[row_num * 2]
                 self.row_odd = self.index_reg[(row_num * 2) + 1]
 
         @A12clk2
         @M12clk2
-        def _():
+        def _():    #  Save working row to the register array
             if self.inst.sc:
                 self.index_reg[self.row_num * 2] = self.row_even
                 self.index_reg[(self.row_num * 2) + 1] = self.row_odd
 
-        @M22clk2
+        @M22clk2    # Read OPA from te data bus and set the working row.
         def _():
             if self.inst.sc:
                 self.row_num = self.data.v >> 1
@@ -54,38 +49,38 @@ class scratch:
         @A32clk1
         @X12clk1
         @X22clk1
-        @X32clk1
+        @X32clk1    # Sample data from the bus at these times.
         def _():
             self.data_in = self.data.v
 
 
+    # Enable the working register (according to whether OPA is even or odd) to the bus.
     def enableReg(self):
         self.data.v = self.row_even if self.inst.opa_even() else self.row_odd
 
+    # Enable the even working register to the bus.
     def enableRegPairH(self):
         self.data.v = self.row_even
 
+    # Enable the odd working register to the bus.
     def enableRegPairL(self):
         self.data.v = self.row_odd
 
+    # Set the proper working register value from data_in.
     def setReg(self):
         if self.inst.opa_even():
             self.row_even = self.data_in
         else:
             self.row_odd = self.data_in
 
+    # Set the even (high) working register from data_in
     def setRegPairH(self):
-        self.row_even = self.data.v
+        self.row_even = self.data_in
 
+    # Set the odd (low) working register from data_in
     def setRegPairL(self):
-        self.row_odd = self.data.v
+        self.row_odd = self.data_in
 
-    # TODO: This should be replaced by addZero when the condition ff is figured out
-    def regZero(self):
-        if self.inst.opa_even():
-            return 1 if self.row_even == 0 else 0
-        else:
-            return 1 if self.row_odd == 0 else 0
 
     def dump(self):
         print("INDEX:{}".format("".join(["{:x}".format(x) for x in self.index_reg])), end='')
