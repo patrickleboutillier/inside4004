612821b1862e8b21caf7de44293e004b9349df77
Patrick LeBoutillier <patrick.leboutillier@gmail.com>
2021-10-05 07:49:21 -0400

    logical parts removed

--- 141-PF/mcs4.py
+++ 141-PF/mcs4.py
@@ -16,18 +16,19 @@ CPU = MCS4.CPU
 sync = CPU.sync
 
 # Create 5 ROMs
-PROM = [i4001.i4001(0, 0, clk1, clk2, sync, data, cm_rom), i4001.i4001(1, 1, clk1, clk2, sync, data, cm_rom), i4001.i4001(2, 0, clk1, clk2, sync, data, cm_rom)] 
+PROM = [i4001.i4001(0, 0, clk1, clk2, sync, data, cm_rom), i4001.i4001(1, 1, clk1, clk2, sync, data, cm_rom)]
+    # i4001.i4001(2, 0, clk1, clk2, sync, data, cm_rom)] 
     # i4001.i4001(3, 0, clk1, clk2, sync, data, cm_rom), i4001.i4001(4, 0, clk1, clk2, sync, data, cm_rom)]
 for r in PROM:
     MCS4.addROM(r)
 
 # Create 2 RAMS
-RAM = [i4002.i4002(0, 0, clk1, clk2, sync, data, cm_ram), i4002.i4002(0, 1, clk1, clk2, sync, data, cm_ram)]
+RAM = [i4002.i4002(0, 0, clk1, clk2, sync, data, cm_ram)] #, i4002.i4002(0, 1, clk1, clk2, sync, data, cm_ram)]
 for r in RAM:
     MCS4.addRAM(0, r)
 
 # Lights
-lights = lights.lights(memory=RAM[1].output.pwire(0), overflow=RAM[1].output.pwire(1), negative=RAM[1].output.pwire(2))
+# lights = lights.lights(memory=RAM[1].output.pwire(0), overflow=RAM[1].output.pwire(1), negative=RAM[1].output.pwire(2))
 
 # Create keyboard 4003
 kbdsr = i4003.i4003(name="KB", clock=PROM[0].io.pwire(0), data_in=PROM[0].io.pwire(1), enable=pwire(1))
@@ -47,18 +48,18 @@ if kb is not None:
 
 # Create printer 4003s
 # Order important here to void race conditions
-psr2 = i4003.i4003(name="P2", clock=PROM[0].io.pwire(2), data_in=PROM[0].io.pwire(1), enable=pwire(1))
-psr1 = i4003.i4003(name="P1", clock=PROM[0].io.pwire(2), data_in=psr2.serial_out, enable=pwire(1))
-MCS4.addSR(psr1)
-MCS4.addSR(psr2)
+# psr2 = i4003.i4003(name="P2", clock=PROM[0].io.pwire(2), data_in=PROM[0].io.pwire(1), enable=pwire(1))
+# psr1 = i4003.i4003(name="P1", clock=PROM[0].io.pwire(2), data_in=psr2.serial_out, enable=pwire(1))
+# MCS4.addSR(psr1)
+# MCS4.addSR(psr2)
 
 # Printer
-printer = printer.printer(fire=RAM[0].output.pwire(1), advance=RAM[0].output.pwire(3), color=RAM[0].output.pwire(0))
-for i in range(10):
-    pbuf(psr2.parallel_out.pwire(i), printer.input.pwire(i))
-    pbuf(psr1.parallel_out.pwire(i), printer.input.pwire(10+i))
+# printer = printer.printer(fire=RAM[0].output.pwire(1), advance=RAM[0].output.pwire(3), color=RAM[0].output.pwire(0))
+# for i in range(10):
+#     pbuf(psr2.parallel_out.pwire(i), printer.input.pwire(i))
+#     pbuf(psr1.parallel_out.pwire(i), printer.input.pwire(10+i))
 # pbuf(printer.sector, test)
-pbuf(printer.index, PROM[2].io.pwire(0))
+# pbuf(printer.index, PROM[2].io.pwire(0))
 
 
 # Load the program
@@ -72,7 +73,7 @@ kb_toggle = False
 
 def callback(nb):
     global step, kb_toggle, MCS4
-    printer.doCycle()
+    # printer.doCycle()
 
     if CPU.addr.isPCin([0x003]) and RAM[0].status[0][3] == 0:   # Before keyboard scanning in main loop, and a button is not currently held down)
         keyboard.clearAdvance()                                 # In case we "pressed" the paper advance button
